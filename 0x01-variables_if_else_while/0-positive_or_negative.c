a68d6b24ff6df6c01810826323fc284dc0dd9b2c
ArkDevLarry <akereleayomide106@gmail.com>
2023-10-06 11:55:56 -0700

    First if...else statement

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,9 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/* more headers goes there */
 
-/* betty style doc for function main goes there */
+
+/**
+ * main - assigns a random number to int n everytime
+ * it executes, and prints it
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
@@ -11,11 +15,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n < 0) {
-	    printf('%d\n is negative', n);
+	    printf('%d is negative\n', n);
 	} elseif (n == 0) {
-	    printf('%d\n is zero', n);
+	    printf('%d is zero\n', n);
 	} else {
-	    printf('%d\n is positive', n);
+	    printf('%d is positive\n', n);
 	}
 	return (0);
 }
