3736eb1713cc2776ed7ee63bd35a12b2b514dc1d
ArkDevLarry <akereleayomide106@gmail.com>
2023-10-07 00:24:30 -0700

    Prints all possible combinations of two two-digit numbers. Ranging btw 0 to 99

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+
+int main (void) {
+
+	int dg1, dg2, dg3, dg4;
+
+	for (dg1 = 0; dg1 <= 9; dg1++)
+	{
+		for (dg2 = 0; dg2 <= 9; dg2++)
+		{
+
+			for (dg3 = 0; dg3 <= 9; dg3++)
+			{
+				for (dg4 = 1; dg4 <=9; dg4++)
+				{
+					putchar(dg1);
+					putchar(dg2);
+					putchar(' ');
+					putchar(dg3);
+					putchar(dg4);
+
+					if (dg1 == 9 && dg2 == 8 && dg3 == 9 && dg4 ==9)
+						continue;
+					putchar(',');
+					puthcar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n')
+	return (0);
+}
