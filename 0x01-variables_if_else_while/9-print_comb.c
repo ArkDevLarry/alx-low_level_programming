21756b3dc78c098b00b83126e06bf68dd1693512
ArkDevLarry <akereleayomide106@gmail.com>
2023-10-07 00:44:09 -0700

    Prints all possible combinations of single-digit numbers.

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,11 +11,10 @@ int main(void)
 	for (ch = 0; ch <= 10; ch++)
 	{
 		putchar((ch % 10) + '0');
-		if (ch !== 9)
-		{
-			putchar(',');
-			putchar(' ');
-		}
+		if (ch == 9)
+			continue;
+		putchar(',');
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
