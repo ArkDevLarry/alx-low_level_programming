5455336f2e477bfb6ed078feda728a859a54108d
ArkDevLarry <akereleayomide106@gmail.com>
2023-10-13 13:05:06 -0700

    Update to project

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,10 +1,16 @@
 #include "main.h"
 
 /**
- * _mul - multiplies 2 digit
- * Print: Product of the 2 digits
+ * mul - multiplies two integers
+ * @a: int to be multiplied to b
+ * @b: int to be multiplied to a
+ * Return: the result of the operation
  */
 int mul(int a, int b)
 {
-	return (a * b);
+	int c;
+
+	c = a * b;
+
+	return (c);
 }
