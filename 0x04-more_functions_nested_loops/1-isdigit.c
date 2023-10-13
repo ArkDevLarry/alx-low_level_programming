fe349373aeaf04d96ff343feeeeccb78c191a64f
ArkDevLarry <akereleayomide106@gmail.com>
2023-10-13 12:49:08 -0700

    Update to project

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for digit
+ * @n: the input to be checked
+ * Return: 1 if n is digit, 0 otherwise
+ */
+int _isdigit(int n)
+{
+	return (n >= 0 && n <= 9);
+}
