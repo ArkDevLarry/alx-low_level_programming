bc20fdc8b2410a02956c421558ab2ba6f9682d8d
Quadri Akerele <akereleayomide106@gmail.com>
2024-01-08 06:29:24 -0800

    Converting bin to uint

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/*
+* * Convert binary to unsigned integer
+*/
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int n = 0;
+	int l = 0;
+	if (b[l] == '\0')
+		return(0);
+
+	while (b[l] == "0" || b[l] == "1")
+		n <<= 1;
+		n += b[l] - "0";
+		l++;
+	
+	return n;
+}
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,1 @@
+Update to bit manipulation
